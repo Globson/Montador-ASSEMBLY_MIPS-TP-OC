0d98228e4e762130a82c5703c7dcaed33f316be9
PabloVizard <pabloferreiraufv@gmail.com>
2019-04-24 16:02:27 -0300

    TP Pronto, falta só adicionar mais pseudo funções para ganhar pontos extras

--- Montador.c
+++ Montador.c
@@ -5,8 +5,9 @@
 
 int main(int argc, char const *argv[]){
   FILE *f = NULL,*saida;
-  char nome_arquivo[20],operacao[4],registrador1[5],registrador2[5],registrador3[5];
+  char nome_arquivo[20],*operacao,registrador1[5],registrador2[5],registrador3[5];
   int valor;
+  operacao = (char*)malloc(sizeof(char));
   while(f == NULL){
     printf("\nDigite o nome do arquivo que deseja abrir:");
     //scanf("%s",nome_arquivo);
@@ -19,6 +20,7 @@ int main(int argc, char const *argv[]){
   printf("\n\tArquivo aberto com sucesso!\n");
   while(!feof(f)){
     fscanf(f,"%s",operacao);
+    //operacao[3]='\0';
     if(strcmp(operacao,"add")==0){
       fprintf(saida, "000000");
       printf("Operação ADD chamada!\n");
@@ -31,6 +33,7 @@ int main(int argc, char const *argv[]){
       Fprintf_registrador(registrador1,saida);
       fprintf(saida, "00000100000\n");
       printf("Registradores: (%s)  (%s)  (%s)\n",registrador1,registrador2,registrador3);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"sub")==0){
       printf("Operação SUB chamada!\n");
@@ -44,6 +47,7 @@ int main(int argc, char const *argv[]){
       Fprintf_registrador(registrador1,saida);
       fprintf(saida, "00000100010\n");
       printf("Registradores: (%s)  (%s)  (%s)\n",registrador1,registrador2,registrador3);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"and")==0){
       printf("Operação AND chamada!\n");
@@ -57,6 +61,7 @@ int main(int argc, char const *argv[]){
       Fprintf_registrador(registrador1,saida);
       fprintf(saida, "00000100100\n");
       printf("Registradores: (%s)  (%s)  (%s)\n",registrador1,registrador2,registrador3);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"or")==0){
       printf("Operação OR chamada!\n");
@@ -70,6 +75,7 @@ int main(int argc, char const *argv[]){
       Fprintf_registrador(registrador1,saida);
       fprintf(saida, "00000100101\n");
       printf("Registradores: (%s)  (%s)  (%s)\n",registrador1,registrador2,registrador3);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"nor")==0){
       printf("Operação NOR chamada!\n");
@@ -83,6 +89,7 @@ int main(int argc, char const *argv[]){
       Fprintf_registrador(registrador1,saida);
       fprintf(saida, "00000100111\n");
       printf("Registradores: (%s)  (%s)  (%s)\n",registrador1,registrador2,registrador3);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"addi")==0){
       char aux[16];
@@ -104,6 +111,7 @@ int main(int argc, char const *argv[]){
       Fprintf_registrador(registrador1,saida);
       fprintf(saida,"%s\n",aux);
       printf("Registradores: (%s)  (%s)  Valor: (%d)\n",registrador1,registrador2,valor);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"andi")==0){
       char aux[16];
@@ -125,8 +133,10 @@ int main(int argc, char const *argv[]){
       Fprintf_registrador(registrador1,saida);
       fprintf(saida,"%s\n",aux);
       printf("Registradores: (%s)  (%s)  Valor: (%d)\n",registrador1,registrador2,valor);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"ori")==0){
+
       char aux[16];
       printf("Operação ORI chamada!\n");
       fprintf(saida, "001101");
@@ -146,6 +156,7 @@ int main(int argc, char const *argv[]){
       Fprintf_registrador(registrador1,saida);
       fprintf(saida,"%s\n",aux);
       printf("Registradores: (%s)  (%s)  Valor: (%d)\n",registrador1,registrador2,valor);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"sll")==0){
       char aux[16];
@@ -160,6 +171,7 @@ int main(int argc, char const *argv[]){
       fprintf(saida,"%s",aux);
       fprintf(saida, "000000\n");
       printf("Registradores: (%s)  (%s)  Valor: (%d)\n",registrador1,registrador2,valor);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"srl")==0){
       char aux[16];
@@ -174,6 +186,7 @@ int main(int argc, char const *argv[]){
       fprintf(saida,"%s",aux);
       fprintf(saida, "000010\n");
       printf("Registradores (%s)  (%s)  Valor: (%d)\n",registrador1,registrador2,valor);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"sw")==0){
       printf("Operação SW chamada!\n");
@@ -187,6 +200,7 @@ int main(int argc, char const *argv[]){
       strcpy(aux,Or_Bits_finais(valor));
       fprintf(saida,"%s\n",aux);
       printf("Registradores (%s)  (%s)  Valor: (%d)\n",registrador1,registrador2,valor);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"lw")==0){
       printf("Operação LW chamada!\n");
@@ -200,6 +214,7 @@ int main(int argc, char const *argv[]){
       strcpy(aux,Or_Bits_finais(valor));
       fprintf(saida,"%s\n",aux);
       printf("Registradores (%s)  (%s)  Valor: (%d)\n",registrador1,registrador2,valor);
+      operacao = (char*)malloc(sizeof(char));
     }
     else if(strcmp(operacao,"move")==0){
       fprintf(saida, "000000");
@@ -214,6 +229,51 @@ int main(int argc, char const *argv[]){
       Fprintf_registrador(registrador1,saida);
       fprintf(saida, "00000100000\n");
       printf("Registradores: (%s)  (%s)\n",registrador1,registrador2);
+      operacao = (char*)malloc(sizeof(char));
+    }
+    else if(strcmp(operacao,"bne")==0){
+      char aux[16];
+      fprintf(saida, "000101");
+      printf("Operação BNE chamada!\n");
+      fscanf(f," %s %s %d",registrador1,registrador2,&valor);
+      registrador1[3]='\0';
+      registrador2[3]='\0';
+      if(valor<0){
+        valor=valor*(-1);
+        strcpy(aux,Or_Bits_finais(valor));
+        strcpy(aux,Complemento2(aux));
+        valor=valor*(-1);
+      }
+      else{
+        strcpy(aux,Or_Bits_finais(valor));
+      }
+      Fprintf_registrador(registrador1,saida);
+      Fprintf_registrador(registrador2,saida);
+      fprintf(saida,"%s\n",aux);
+      printf("Registradores: (%s)  (%s)  Valor: (%d)\n",registrador1,registrador2,valor);
+      operacao = (char*)malloc(sizeof(char));
+    }
+    else if(strcmp(operacao,"beq")==0){
+      char aux[16];
+      fprintf(saida, "000100");
+      printf("Operação BEQ chamada!\n");
+      fscanf(f," %s %s %d",registrador1,registrador2,&valor);
+      registrador1[3]='\0';
+      registrador2[3]='\0';
+      if(valor<0){
+        valor=valor*(-1);
+        strcpy(aux,Or_Bits_finais(valor));
+        strcpy(aux,Complemento2(aux));
+        valor=valor*(-1);
+      }
+      else{
+        strcpy(aux,Or_Bits_finais(valor));
+      }
+      Fprintf_registrador(registrador1,saida);
+      Fprintf_registrador(registrador2,saida);
+      fprintf(saida,"%s\n",aux);
+      printf("Registradores: (%s)  (%s)  Valor: (%d)\n",registrador1,registrador2,valor);
+      operacao = (char*)malloc(sizeof(char));
     }
   }
   fclose(f);
